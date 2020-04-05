f0fd477ff92b492005cbd0af98329c4deb1c6f16
Remy EUSEBE <remyeusebe@gmail.com>
2020-04-05 01:11:37 +0000

    init

---
+++ back/src/constants/rules.ts
@@ -0,0 +1,281 @@
+export default [
+  {
+    rule: '1 pour tous, Tous pour 1 : Tout le monde trinque et bois une gorgée.',
+    number: 1,
+  },
+  {
+    rule:
+      "2 d'Tension: T'as cru qu'c'est en faisant des 2 que t'allais gagner...Allez bois 2 gorgées",
+    number: 2,
+  },
+  {
+    rule:
+      "Cheval de 3 : Un poison s'est dissimulé dans ton breuvage...Rajoute de la potion dans ton verre",
+    number: 3,
+  },
+  {
+    rule:
+      'Péte à 4 feuilles: Tu gagnes un joker qui annulera le gage de ton choix à utiliser avant la fin de la partie.',
+    number: 4,
+  },
+  {
+    rule:
+      "Unis comme les 5 foigts de la main : L'union fais la force. Tout le monde boit une gorgée",
+    number: 5,
+  },
+  {
+    rule:
+      "6 pieds sous terre : Extermine ton adversaire. Fais boire 6 gorgées à l'adversaire de ton choix.",
+    number: 6,
+  },
+  {
+    rule:
+      'Bottes de 7 lieues : Tuviens de voler les chaussures de Flash Gordon. Avance 1 case devant le joueur de tête. Si tu es déjà premier, rejoue.',
+    number: 7,
+  },
+  {
+    rule: 'Grand 8 : Bois 2 Gorgées et fais 8 tours sur toi-même.',
+    number: 8,
+  },
+  {
+    rule:
+      "Les 9 vies d'un chat: Chaque joueur lance le dé.Le plus petit score se fait écraser et retourne sur la case départ.Si tu avais un joker dit- lui adieu.",
+    number: 9,
+  },
+  {
+    rule:
+      '1 de perdue, 10 de retrouvées. Tu as misé sur le mauvais cheval. Bois 2 gorgées et rejoue',
+    number: 10,
+  },
+  {
+    rule: '11 Septembre : Tu as le bonjour de Ben Laden. Passe 2 tours.',
+    number: 11,
+  },
+  {
+    rule:
+      "Les 12 coups de minuit : L'heure du crime...Lance le dé et fais boire le nombre de gorgées qu'il t'indiquera au joueur de ton choix.",
+    number: 12,
+  },
+  {
+    rule:
+      "Vendredi 13 : Tous tes malheurs sont doublés jusqu'à la fin de la partie, sauf si tu meurs avant.",
+    number: 13,
+  },
+  {
+    rule:
+      "Chercher midi à 14 heures : Une brèche spatio-temporrelle vient de s'ouvrir sous vos pieds.L'ordre du lancé du dé s'inverse.",
+    number: 14,
+  },
+  {
+    rule:
+      "Samu 15 : C'est parti pour un tout d'ambulance. Lance le dé et recule du nombre de cases qu'il t'indiquera.",
+    number: 15,
+  },
+  {
+    rule: "Alors comme ça tu vis dans le 16 : Ici c'est le Gouffre...Bois 2 gorgées.",
+    number: 16,
+  },
+  {
+    rule:
+      '17 G.A.V : Ton voisin a appelé les keufs. Passe un tour, et si tu as un joker tu le perds à la fouille.',
+    number: 17,
+  },
+  {
+    rule: "18 : Interdit aux moins de 18 : Le dernier à mettre son majeur en l'air bois 2 gorgées.",
+    number: 18,
+  },
+  {
+    rule:
+      '19 : En verlan ça fait 9.1 : Représente juste pour nois-même. Tout le monde boit 1 gorgée sauf toi.',
+    number: 19,
+  },
+  {
+    rule: 'Qui connait Mr Tilitre ??? Vincent Tilitre : Finis ton verre.',
+    number: 20,
+  },
+  {
+    rule: '4.21 : Défi de dés. Tout le monde lance le dé, le plus petit score boit 2 gorgées.',
+    number: 21,
+  },
+  {
+    rule:
+      "22 V'là les flics: course - poursuite!!! Tous les joueurs lancent le dé, le plus petit score se fait péter et finit en G.A.V case 17.",
+    number: 22,
+  },
+  {
+    rule: 'Jamais 2 sans 3 : Bois 2 gorgées et offre la 3éme au joueur de ton choix.',
+    number: 23,
+  },
+  {
+    rule:
+      '24 Heures du Mans : Tes adversaires sont au stand, double-les. Avance une case devant le joueur qui te devances. Si tu es en tête, rejoue.',
+    number: 24,
+  },
+  {
+    rule:
+      "25 Décembre : c'est Noël, mais dans le Gouffre rare sont les cadeaux...Lance le dé, si tu fais un 2 ou un 5 tu gagnes un joker.",
+    number: 25,
+  },
+  {
+    rule:
+      "26 lettres de l'alphabet: Choisis une lettre, le 1er à trouver le nom d'un rappeur commençant par celle-ci fait boire 2 gorgées au joueur de son choix.",
+    number: 26,
+  },
+  {
+    rule:
+      "Club des 27 : La mort t' as fauché en pleine gloire, retournes à la case départ...Si tu avais un joker il passe à la trappe.",
+    number: 27,
+  },
+  {
+    rule: '28 jours plus tard : La mutation est en cours... Bois 2 gorgées.',
+    number: 28,
+  },
+  {
+    rule: '29 Février : Tu fêtes ton anniversaire tous les 4 ans. Passe 3 tours.',
+    number: 29,
+  },
+  {
+    rule:
+      "30 Millions d'énnemis: Tous les joueurs lancent le dé sauf toi, celui ou ceux qui feront un 3 iront sur la case 13 et en paieront les conséquences...",
+    number: 30,
+  },
+  { rule: "Tu t'es mis sur ton 31: tout le monde boit 1 gorgee sauf toi", number: 31 },
+  {
+    rule:
+      'Souris ! Montre nous tes 32 dents : Changement de corps. Change de pion avec le joueur de ton choix. Tu herites de sa case, de ses jokers comme de ses poisses, et vice-versa',
+    number: 32,
+  },
+  { rule: '33 Tours : Tu tournes en rond comme un vinyle. Bois 1 gorgee', number: 33 },
+  {
+    rule:
+      '3/4 Cuir : Fusillade !!! Choisis ton bouclier humain. Le joueur de ton choix se fait descendre et retournes sur la case depart. PS: Fouilles-le, voir si il a pas un joker',
+    number: 34,
+  },
+  {
+    rule:
+      "Les 35 heures c'est fini: Il est l'heure de faire des heures sup... Lance le de et bois le nombre de gorgees qu'il indiquera",
+    number: 35,
+  },
+  {
+    rule:
+      "36ieme Chambre de Shaolin: ODB revient des morts te donner son joker qu'il n'a pas eu le temps d'utiliser.. Tu gagnes un joker a utiliser avant la fin de la partie",
+    number: 36,
+  },
+  { rule: '37,5 degres: La temperature monte... Bois 1 bouchon', number: 37 },
+  { rule: 'Amandine du 38: Chante quelques phases du son de ton choix', number: 38 },
+  {
+    rule:
+      "Debut de la guerre 39-45: Attends qu'un joueur fasse un 6 pour que la guerre se finisse et que tu ailles sur la case 45 sinon tu bois une gorgeee a chaque defaite, c'est a dire a chaque fois que quelqu'un lance le de.",
+    number: 39,
+  },
+  {
+    rule:
+      'Ali Baba et les 40 voleurs: Si un des joueurs a un joker tu peux lui voler ... Sinon tant pis pour toi, bois 2 gorgees.',
+    number: 40,
+  },
+  {
+    rule:
+      "4-1 Catin : on t'a pete les reins, tu boites.... Le samu est venu te chercher, retourne sur la case 15.",
+    number: 41,
+  },
+  {
+    rule:
+      "Marathon. 42km: La course est loin d'etre finie, prends des forces on t'offre a boire. Bois 2 gorgees dans le verre de l'adversaire de ton choix",
+    number: 42,
+  },
+  {
+    rule:
+      "Georges W.Bush 43ieme president des Etats-Unis: Bouton Rouge ! Arme de destruction massive ! Tous tes adversaires lancent le de et boivent le nombre de gorgees qu'il indiquera.",
+    number: 43,
+  },
+  { rule: '4x4 : Va sur la case du joueur de ton choix.', number: 44 },
+  { rule: "Fin de la guerre 49-45: Arministice, Tout l'monde boit 1 gorgee.", number: 45 },
+  {
+    rule:
+      'En 2-4-6 : Chaque joueur lance le de, celui qui ne fait pas un 2, un 4 ou un 6 boit 2 gorgees.',
+    number: 46,
+  },
+  {
+    rule:
+      "AK-47: Tire dans l'tas. Chacun de tes adversaires choisit un chiffre de 1 a 6 (ils peuvent choisir le meme). Lance le de, si le chiffre d'un des joueurs sort, tu l'executes et il devra retourner a la case depart. Sinon, tu vas en G.A.V case 48.",
+    number: 47,
+  },
+  {
+    rule:
+      "48 heures de G.A.V: La garde a vue s'eternise... Passe 2 tours. Si tu as un joker, tu le perds a la fouille.",
+    number: 48,
+  },
+  {
+    rule:
+      "Marche Arriere: Tout l'monde recule d'1 case et subit le gage de la case sur laquelle il atterit.",
+    number: 49,
+  },
+  {
+    rule:
+      "Top 50: Fredonne un air connu, le premier a trouver le titre ou l'interprete fais boire 3 gorgees a l'adversaire de son choix",
+    number: 50,
+  },
+  { rule: 'Zone 51: Tu as ete enleve par les extra-terrestres. Passe 1 tour.', number: 51 },
+  {
+    rule:
+      "Coup d'poker 52 cartes: Tapis. Choisis le joueur de ton choix pour une partie de pierre-feuille-ciseaux. Le perdant devra finir son verre.",
+    number: 52,
+  },
+  {
+    rule:
+      "Coccinelle n* 53: Panne seche, va chercher d'l'essance. Lance le de et recule du nombre de case qu'il t'indiquera",
+    number: 53,
+  },
+  {
+    rule:
+      "5 4 3 2 1 Feuuuuuu: Tout l'monde boit une gorgee. Le dernier a la boire en reboit 2 autres.",
+    number: 54,
+  },
+  { rule: "Recu 5/5: Distribue 5 gorgees a l'adversaire de ton choix", number: 55 },
+  { rule: '56 kbs/s: Putain tu rames mec, ici on boit a haut debit. Bois 3 gorgees.', number: 56 },
+  {
+    rule:
+      "3-5-7 Magnum: Roulette russe... Tous les joueurs lancent le de, le 1er a faire 1 s'en prend une en pleine tete, et retourne a la case depart.",
+    number: 57,
+  },
+  {
+    rule: '58 minutes pour vivre: Tu es invincible comme John Mac Clane. TU gagnes un joker',
+    number: 58,
+  },
+  { rule: "59 bienvenues chez les ch'tis: Bois 1 gorgee a la bouteille.", number: 59 },
+  {
+    rule:
+      "60 secondes chrono: Minute de silence. Tu n'asplus l'droit a la parole durant 1 minute, sinon finis ton verre cul sec.",
+    number: 60,
+  },
+  {
+    rule:
+      '6-1/6-1: Jeu set et match: Victoire ecrasante. Fais boire 2 gorgees au joueur de ton choix.',
+    number: 61,
+  },
+  { rule: 'PUTE', number: 62 },
+  {
+    rule:
+      '28 aout 1963: I have a dream... Chez nous les reves se finissent toujours en cauchemar... Avance de 6 cases.',
+    number: 63,
+  },
+  {
+    rule: "64 cases en moins: Echec et mat. Le gouffre t'as vaincu. Avance de 5 cases.",
+    number: 64,
+  },
+  {
+    rule: "65 ans: L'age de la retraite: Dans l'Gouffre on ne l'atteint pas... Avance de 4 cases.",
+    number: 65,
+  },
+  { rule: 'Route 66: Elle te guide vers ta decadence. Avance de 3 cases.', number: 66 },
+  { rule: "6+7 = 13 : La poisse t'as rattrape. Retourne sur la case 13", number: 67 },
+  {
+    rule: "Mai 68: Toute revolution est inutile. Tu ne pas changer ta destinee. Avance d'1 case.",
+    number: 68,
+  },
+  {
+    rule:
+      "Le gouffre te la fait toujours a l'envers. \n On ne peut pas sortir du Gouffre, fais toi une raison ... \n Finis ton verre et savoure la defaite !",
+    number: 69,
+  },
+];
